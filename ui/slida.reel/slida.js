6fbb37d79ce0382ff98971e378880e27bfc3a989
Wolfram Kriesing <wk+github@uxebu.com>
2014-07-05 22:39:12 +0000

    Add component slida

---
+++ ui/slida.reel/slida.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/slida.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Slida
+ * @extends Component
+ */
+exports.Slida = Component.specialize(/** @lends Slida# */ {
+    constructor: {
+        value: function Slida() {
+            this.super();
+        }
+    }
+});
